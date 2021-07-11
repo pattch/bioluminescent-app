7904ed0702fbebdc142fae8857fe1ee8fb710a67
Samuel Sampson <innovatests@gmail.com>
2021-07-11 21:25:01 +0000

    Initialize Album Card Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AlbumCardComponent } from './album-card/album-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlbumCardComponent
   ],
   imports: [
     BrowserModule
